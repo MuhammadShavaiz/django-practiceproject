11612681a5fae3cf174345ece6e4039d57cc567e
Muhammad Shavaiz <shavaizsohail@gmail.com>
2024-06-14 08:01:09 -0700

    initial commit

--- ecommerce_shop/models.py
+++ ecommerce_shop/models.py
@@ -4,3 +4,7 @@ class Product(models.Model):
     product_name = models.CharField(max_length=50)
     desc = models.CharField(max_length=300)
     pub_date = models.DateField()
+    prod_img = models.ImageField(upload_to='ecommerce_shop/images', default="")
+
+    def __str__(self):
+        return self.product_name
