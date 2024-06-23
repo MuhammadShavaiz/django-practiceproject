e616abdcb35f59be6e7a194f2bf744ac2607be5f
Muhammad Shavaiz <shavaizsohail@gmail.com>
2024-06-23 03:22:37 -0700

    initial commit

--- ecommerce_shop/views.py
+++ ecommerce_shop/views.py
@@ -1,8 +1,21 @@
+from math import ceil
 from django.shortcuts import render
 from django.http import HttpResponse
+from ecommerce_shop.models import Product
 
 def home(request):
-    return render(request, 'home.html')
+    products= Product.objects.all()
+    allProds=[]
+    catprods= Product.objects.values('category', 'id')
+    cats= {item["category"] for item in catprods}
+    for cat in cats:
+        prod=Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+
+    params={'allProds':allProds }
+    return render(request,'home.html', params)
 def about(request):
     return render(request, "about.html")
 def contact(request):
